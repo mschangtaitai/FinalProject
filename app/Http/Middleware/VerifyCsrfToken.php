3576b434c510923cb777f8cc2a1117ae00175dc4
mschangtaitai <mschangtaitai@gmail.com>
2023-03-13 20:37:15 -0600

    make endpoints accesible

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'api/register',
-        'api/sanctum/token'
+        'api/sanctum/token',
+        'api/*'
     ];
 }
